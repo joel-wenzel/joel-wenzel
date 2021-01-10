f90ac49253078fa4b7b12f401da364003271440e
Joel <wenzje07@gmail.com>
2021-01-10 08:53:40 -0600

    ng universal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
@@ -12,6 +13,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
+const config = {
+  apiKey: "AIzaSyDl7m6fkOooFIya9Trb2NwOJsmkzApIVTc",
+  authDomain: "joelwenzel-45c43.firebaseapp.com",
+  projectId: "joelwenzel-45c43",
+  storageBucket: "joelwenzel-45c43.appspot.com",
+  messagingSenderId: "642544165921",
+  appId: "1:642544165921:web:1a335e14e685694677ef60",
+  measurementId: "G-EG8T8945K9"
+}
 
 @NgModule({
   declarations: [
@@ -19,7 +29,8 @@ import { HomeComponent } from './home/home.component';
     HomeComponent
   ],
   imports: [
-    BrowserModule,
+    AngularFireModule.initializeApp(config),
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
     NoopAnimationsModule,
     HttpClientModule,
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private title: Title, private meta: Meta) { }
 
   ngOnInit(): void {
+    this.title.setTitle('Joel Wenzel');
+    this.meta.addTags([
+      { name: 'og.url', content: '/home'},
+      { name: 'og.title', content: 'Joel Wenzel'},
+      { name: 'og.description', content: 'lorem'},
+      { name: 'og.image', content: '/home'},
+    ])
   }
 
 }
